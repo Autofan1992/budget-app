484ef057eeb2be514ffcfd11f1c76c59bc1fb4bf
Autofan <kolya.gordii@gmail.com>
2022-05-15 18:56:15 +0300

    injected formik to add expense modal form

--- src/components/Expenses/AddExpenseModal.tsx
+++ src/components/Expenses/AddExpenseModal.tsx
@@ -1,67 +1,92 @@
 import { Button, Form, Modal } from 'react-bootstrap'
-import { ChangeEvent, Dispatch, FC, FormEvent, SetStateAction, useEffect, useRef, useState } from 'react'
+import { Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from '../../context/budgets-context'
-import { BudgetType } from '../../types/types'
+import { BudgetType, ExpenseType } from '../../types/types'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+import { Omit } from 'react-bootstrap/helpers'
+
+const expenseSchema = Yup.object().shape({
+    description: Yup.string()
+        .required('Description is required')
+        .max(200, 'Description can\'t be longer than 200 characters')
+        .min(2, 'Description must have at least 2 characters')
+})
 
 const AddExpenseModal: FC<PropsType> = ({ show, handleClose, selectedBudget }) => {
     const handleModalHide = () => handleClose(false)
     const { addExpense, budgets } = useBudgets()
-    const budgetRefId = useRef<HTMLSelectElement>(null)
-    const descRef = useRef<HTMLInputElement>(null)
-    const amountRef = useRef<HTMLInputElement>(null)
     const [selectedBudgetName, setBudgetName] = useState<string>()
-    const [validated, setValidated] = useState(false)
 
     useEffect(() => {
         setBudgetName(selectedBudget.title)
     }, [selectedBudget])
 
-    const handleSelectedBudgetName = (e: ChangeEvent<HTMLSelectElement>) => {
-        setBudgetName(e.target.options[e.target.selectedIndex].text)
-    }
-
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-
-        if (!e.currentTarget.checkValidity()) {
-            e.stopPropagation()
-        } else if (amountRef.current && budgetRefId.current && descRef.current) {
-            addExpense({
-                description: descRef.current.value,
-                amount: +amountRef.current.value,
-                budgetId: budgetRefId.current.value
-            })
-            handleModalHide()
-        }
-        setValidated(true)
-    }
-
     return <Modal show={show} onHide={handleModalHide}>
-        <Form onSubmit={handleSubmit} validated={validated}>
-            <Modal.Header closeButton>
-                <Modal.Title>{selectedBudgetName === UNCATEGORIZED_BUDGET_ID ? 'New expense' : 'Add Expense to ' + selectedBudgetName}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Label>Description</Form.Label>
-                    <Form.Control type="text" ref={descRef} required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="description">
-                    <Form.Label>Amount</Form.Label>
-                    <Form.Control type="number" min={0} step={0.01} ref={amountRef} required/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="budget">
-                    <Form.Label>Budget</Form.Label>
-                    <Form.Select onChange={handleSelectedBudgetName} ref={budgetRefId} defaultValue={selectedBudget.id}>
-                        <option value={UNCATEGORIZED_BUDGET_ID}>{UNCATEGORIZED_BUDGET_ID}</option>
-                        {budgets.map(budget => <option key={budget.id} value={budget.id}>{budget.title}</option>)}
-                    </Form.Select>
-                </Form.Group>
-                <div className="text-end">
-                    <Button variant="primary" type="submit">Add Expense</Button>
-                </div>
-            </Modal.Body>
-        </Form>
+        <Modal.Header closeButton>
+            <Modal.Title>{selectedBudgetName === UNCATEGORIZED_BUDGET_ID ? 'New expense' : 'Add Expense to ' + selectedBudgetName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Formik
+                validationSchema={expenseSchema}
+                initialValues={{
+                    description: '',
+                    amount: 0,
+                    budgetId: selectedBudget.id,
+                } as Omit<ExpenseType, 'id'>}
+                onSubmit={({ budgetId, amount, description }, { resetForm, setSubmitting }) => {
+                    addExpense({
+                        description,
+                        amount,
+                        budgetId
+                    })
+                    handleClose(false)
+                    resetForm()
+                    setSubmitting(false)
+                }}
+            >
+                {({ handleSubmit, touched, errors, handleChange, values }) => <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control
+                            className={touched.description && errors.description ? 'is-invalid' : undefined}
+                            placeholder="Type budget description"
+                            onChange={handleChange}
+                            value={values.description}
+                            name="description"
+                            type="text"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control
+                            className={touched.amount && errors.amount ? 'is-invalid' : undefined}
+                            placeholder="Type expense amount"
+                            onChange={handleChange}
+                            value={values.amount}
+                            name="amount"
+                            type="number"
+                            min={1}
+                            step={0.01}
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">{errors.amount}</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Budget</Form.Label>
+                        <Form.Select onChange={handleChange} name='budgetTitle' defaultValue={selectedBudget.id}>
+                            <option value={UNCATEGORIZED_BUDGET_ID}>{UNCATEGORIZED_BUDGET_ID}</option>
+                            {budgets.map(budget => <option key={budget.id} value={budget.id}>{budget.title}</option>)}
+                        </Form.Select>
+                    </Form.Group>
+                    <div className="text-end">
+                        <Button variant="primary" type="submit">Add Expense</Button>
+                    </div>
+                </Form>}
+            </Formik>
+        </Modal.Body>
     </Modal>
 }
 
